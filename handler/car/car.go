180446c1811f15e6ff01880b98b0a20bd35c0ec5
shafnybuilds <shafnymhd@proton.me>
2025-06-23 19:52:38 +0530

    Implements car handler with HTTP endpoints
    Adds HTTP handler implementation for car management operations with the following features:
    - CRUD operations for car entities
    - Query support for filtering by brand and engine status
    - Proper error handling and HTTP status codes
    - JSON request/response handling
    
    Includes Gorilla Mux router dependency for enhanced routing capabilities.

--- handler/car/car.go
+++ handler/car/car.go
@@ -1,1 +1,190 @@
 package car
+
+import (
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/shafnybuilds/car_management_sys/models"
+	"github.com/shafnybuilds/car_management_sys/service"
+)
+
+type CarHandler struct {
+	service service.CarServiceInterface
+}
+
+func NewCarHandler(service service.CarServiceInterface) *CarHandler {
+	return &CarHandler{
+		service: service,
+	}
+}
+
+// method
+func (h *CarHandler) GetCarByID(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	resp, err := h.service.GetCarByID(ctx, id)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println("Error: ", err)
+		return
+	}
+
+	body, err := json.Marshal(resp)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println("Error: ", err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	// response body
+	_, err = w.Write(body)
+	if err != nil {
+		log.Println("Error Writing Response : ", err)
+	}
+}
+
+func (h *CarHandler) GetCarByBrand(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	brand := r.URL.Query().Get("brand")
+	isEngine := r.URL.Query().Get("isEngine") == "true"
+
+	resp, err := h.service.GetCarByBrand(ctx, brand, isEngine)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println("Error: ", err)
+		return
+	}
+
+	body, err := json.Marshal(resp)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println("Error: ", err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	// response body
+	_, err = w.Write(body)
+	if err != nil {
+		log.Println("Error Writing Response : ", err)
+	}
+}
+
+func (h *CarHandler) CreateCar(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		log.Println("Err: ", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	var carReq models.CarRequest
+	err = json.Unmarshal(body, &carReq)
+	if err != nil {
+		log.Println("Error while Unmarshalling Request body", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	createdCar, err := h.service.CreateCar(ctx, &carReq)
+	if err != nil {
+		log.Println("Error Creating Car: ", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	responseBody, err := json.Marshal(createdCar)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println("Error while marshaling: ", err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+
+	// write the response body
+	_, _ = w.Write(responseBody)
+}
+
+func (h *CarHandler) UpdateCar(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	params := mux.Vars(r)
+	id := params["id"]
+
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		log.Println("Error Reading Request Body: ", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	var carReq models.CarRequest
+	err = json.Unmarshal(body, &carReq)
+	if err != nil {
+		log.Println("Error while Unmarshalling Request body", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	updatedCar, err := h.service.UpdateCar(ctx, id, &carReq)
+	if err != nil {
+		log.Println("Error While Upadating the Car: ", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	resBody, err := json.Marshal(updatedCar)
+	if err != nil {
+		log.Println("Error while Marshalling Body: ", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	// write the response body
+	_, _ = w.Write(resBody)
+}
+
+func (h *CarHandler) DeleteCar(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	params := mux.Vars(r)
+	id := params["id"]
+
+	deletedCar, err := h.service.DeleteCar(ctx, id)
+	if err != nil {
+		log.Println("Error While Deleting the Car: ", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	body, err := json.Marshal(deletedCar)
+	if err != nil {
+		log.Println("Error while Marshalling the Response: ", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	// write the response body
+	_, err = w.Write(body)
+	if err != nil {
+		log.Println("Error Writing Response", err)
+	}
+}
--- service/interface.go
+++ service/interface.go
@@ -1,1 +1,15 @@
 package service
+
+import (
+	"context"
+
+	"github.com/shafnybuilds/car_management_sys/models"
+)
+
+type CarServiceInterface interface {
+	GetCarByID(ctx context.Context, id string) (*models.Car, error)
+	GetCarByBrand(ctx context.Context, brand string, isEngine bool) ([]models.Car, error)
+	CreateCar(ctx context.Context, car *models.CarRequest) (*models.Car, error)
+	UpdateCar(ctx context.Context, id string, carReq *models.CarRequest) (*models.Car, error)
+	DeleteCar(ctx context.Context, id string) (*models.Car, error)
+}
